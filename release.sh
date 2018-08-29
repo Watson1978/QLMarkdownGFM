effeabd3c57e99e344bf67e70ecfda0f9b57e048
Watson <watson1978@gmail.com>
2018-08-29 10:37:43 +0900

    fix release script

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf build
-xcodebuild -arch x86_64 -arch i386 -configuration Release
+xcodebuild -workspace QLMarkdownGFM.xcworkspace -scheme QLMarkdownGFM -arch x86_64 -arch i386 -configuration Release
 
 cd build/Release
 zip -r QLMarkdownGFM.qlgenerator.zip QLMarkdownGFM.qlgenerator
