d7db255ef31ecb06639f4dd769171ddb9cf9af50
Watson <watson1978@gmail.com>
2018-09-01 00:35:39 +0900

    add Homebrew formula

---
+++ qlmarkdown-gfm.rb
@@ -0,0 +1,12 @@
+cask 'qlmarkdown-gfm' do
+  version '1.1.0'
+  sha256 '0fbd15bd79ee0d80750982155a70ace1598304bfb2a29d68a0ae71110e213b35'
+
+  url "https://github.com/Watson1978/QLMarkdownGFM/releases/download/v#{version}/QLMarkdownGFM.qlgenerator.zip"
+  appcast 'https://github.com/Watson1978/QLMarkdownGFM/releases.atom',
+          checkpoint: '204c4529f1028cfa06d292641c30b76cb5cb8e9102028d9ced7084db18247a03'
+  name 'QLMarkdownGFM'
+  homepage 'https://github.com/Watson1978/QLMarkdownGFM'
+
+  qlplugin 'QLMarkdownGFM.qlgenerator'
+end
