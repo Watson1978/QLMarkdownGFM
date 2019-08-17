febe050885969f15def97a3fa3f0612e191db12a
Watson <watson1978@gmail.com>
2019-08-18 02:32:52 +0900

    add basic properties to render HTML

--- QLMarkdownGFM/GeneratePreviewForURL.m
+++ QLMarkdownGFM/GeneratePreviewForURL.m
@@ -15,9 +15,12 @@ OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview,
 {
     @autoreleasepool {
         NSData *data = renderHTML((__bridge NSURL*) url);
-        
+        NSDictionary* properties = @{
+            (__bridge NSString*)kQLPreviewPropertyTextEncodingNameKey : @"UTF-8",
+            (__bridge NSString*)kQLPreviewPropertyMIMETypeKey : @"text/html",
+        };
         if (data) {
-            QLPreviewRequestSetDataRepresentation(preview, (__bridge CFDataRef)data, kUTTypeHTML, NULL);
+            QLPreviewRequestSetDataRepresentation(preview, (__bridge CFDataRef)data, kUTTypeHTML, (__bridge CFDictionaryRef)properties);
         }
         
         return noErr;
