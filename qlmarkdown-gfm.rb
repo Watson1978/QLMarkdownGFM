f561b661863b31a16e66644a60b4ad2ec2636a8e
Watson <watson1978@gmail.com>
2019-08-18 02:40:02 +0900

    update formula to v1.2.3

--- qlmarkdown-gfm.rb
+++ qlmarkdown-gfm.rb
@@ -1,6 +1,6 @@
 cask 'qlmarkdown-gfm' do
-  version '1.2.2'
-  sha256 'cb1b9388895aa0adc34f6f0d5100544cdeef5fa9de64d7b42d410af0320e538f'
+  version '1.2.3'
+  sha256 '1b1388601a047c3159f4569691efe69104749c79cb4f83c92dc5c1b1fca98010'
 
   url "https://github.com/Watson1978/QLMarkdownGFM/releases/download/v#{version}/QLMarkdownGFM.qlgenerator.zip"
   appcast 'https://github.com/Watson1978/QLMarkdownGFM/releases.atom',
