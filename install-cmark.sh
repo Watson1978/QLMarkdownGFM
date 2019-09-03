af1b4c6cdabec6d4b7eb58b93785c9a450c791d7
Watson <watson1978@gmail.com>
2019-09-03 22:25:34 +0900

    fix build error

--- install-cmark.sh
+++ install-cmark.sh
@@ -14,6 +14,7 @@ cd build
 cmake -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX="$PROJECT_DIR/cmark" -DCMAKE_OSX_ARCHITECTURES="x86_64" -DCMAKE_OSX_DEPLOYMENT_TARGET="10.11" ..
 make -j
 make install
+cp ./src/config.h $PROJECT_DIR/cmark/include
 
 rm -rf $PROJECT_DIR/cmark/bin
 rm -rf $PROJECT_DIR/cmark/share
